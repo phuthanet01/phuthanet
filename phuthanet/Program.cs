2a37df5ea2a333044d17037e1134355e8364ce49
phuthanet01 <phuthanet.son@sbac.ac.th>
2023-06-07 14:50:41 +0700

    Add project files.

---
+++ phuthanet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace phuthanet01
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            for (int iterator = 0; iterator < 3; iterator++)
+            {
+                Console.WriteLine("Outer Loop:" + iterator);
+                for (int _iterator = 0; _iterator < 2; _iterator++)
+                {
+                    Console.WriteLine("inner Loop:" + iterator);
+
+
+                }
+            }
+            string[] brandTV = { "LG", "SONY", "SAMSUNG", "TCL", "JVC" };
+
+            foreach (string television in brandTV)
+
+                Console.WriteLine("Brand" + television);
+
+
+        }
+    }
+}
+
